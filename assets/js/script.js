a421971b120febb14d01f56e6c0bb52860a63e64
Akshay Kumar <officialakshay71@gmail.com>
2026-03-01 11:21:07 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-(window.onload = function () {
+(function () {
     const menuBtn = document.getElementById('menuBtn');
     const mobileMenu = document.getElementById('mobileMenu');
     if (menuBtn && mobileMenu) {
